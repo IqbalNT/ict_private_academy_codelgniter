9e0a84c4a61bc4a92e147c30b1cc74a033c2bf13
Md. Iqbal Hossain <35366940+IqbalNT@users.noreply.github.com>
2018-02-08 02:26:57 +0600

    Update Registration.php

--- application/models/Registration.php
+++ application/models/Registration.php
@@ -44,4 +44,48 @@ class Registration extends CI_Model
         $this->db->where('id',$uid);
         $this->db->update('tutor',$info);
       }
+
+
+      public function tu_add_course($title)
+      {
+        $this->db->trans_start();
+        $this->db->insert('course',$title);
+        $cid = $this->db->insert_id();
+        $this->db->trans_complete();
+        return $cid;
+      }
+
+      public function show_course()
+      {
+        $tid=$this->session->userdata('id');
+        $this->db->select('*');
+        $this->db->from('course');
+        $this->db->where('tid',$tid);
+        //$this->db->order_by('tid','DESC');
+        $sql=$this->db->get();
+        return $sql->result();
+
+      /*  //for codelgniter table show
+        $tid=$this->session->userdata('id');
+         $this->db->select('id,title');
+        $this->db->from('course');
+        $this->db->where('tid',$tid);
+         $sql=$this->db->get();
+         return $this->table->generate($sql);
+*/
+
+
+      }
+
+      public function tutor_update_course($cid)
+      {
+        $this->db->where('cid',$cid);
+       $this->db->update('course',['title'=>$this->input->post('course_title')]);
+      }
+
+      public function tutor_delete_course($cid)
+      {
+        $this->db->where('cid',$cid);
+       $this->db->delete('course');
+      }
  }
